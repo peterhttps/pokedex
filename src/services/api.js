78be4306712f9a993d17c73453c51490ce2dd734
peterhttps <plrf100@gmail.com>
2020-05-28 14:12:47 -0300

    modify api endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://pokeapi.co/api/v2/pokemon',
+    baseURL: 'http://pokeapi.salestock.net/api/v2/pokemon',
 });
 
 export default api;
